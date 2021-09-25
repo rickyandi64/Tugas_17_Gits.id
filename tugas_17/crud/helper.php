13d9bc50523b84404ccd39f7583e331c260320aa
Rickyandikurniawan64 <39975146+rickyandi64@users.noreply.github.com>
2021-09-25 17:28:30 +0700

    Delete tugas_17/crud directory

--- tugas_17/crud/helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASS','');
-    define('DB', 'db_catatan');
-    
-    $db_connect = mysqli_connect(HOST, USER, PASS, DB ) or die ('Unable Connect');
-    header('Content-Type: application/json')
-?>
